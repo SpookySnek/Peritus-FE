333c4adcd248e5fe858e0d2eb95e925b42456bd2
SpookySnek <carleric.olsson@gmail.com>
2022-09-15 12:59:50 +0200

    Added mantine component library

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import { MantineProvider, Text } from '@mantine/core';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface Quote {
+  id: number,
+  title: string,
+  uploadDate: Date,
+  imgUrl: string,
+  tag: string
 }
 
-export default App;
+
+export default function App() {
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Text>Welcome to Mantine!</Text>
+    </MantineProvider>
+  );
+}
