461870a8c3ca69b54209a6ca94fb745642be9bc2
SpookySnek <carleric.olsson@gmail.com>
2022-09-19 12:23:26 +0200

    Updated App.tsx, added functionality

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,5 +1,9 @@
+import React, { useEffect, useState } from 'react';
 import { MantineProvider, Text } from '@mantine/core';
 import './App.css';
+import AddQuote from '../addQuote/addQuote';
+import QuoteList from '../quoteList/quoteList';
+import axios from 'axios';
 
 export interface Quote {
   id: number,
@@ -8,10 +12,37 @@ export interface Quote {
 }
 
 
-export default function App() {
+
+
+function App() {
+
+  const [quotes, setQuotes] = useState<Quote[]>([]);
+
+  useEffect(() => {
+    const dataFetch = async () => {
+      const resp = await axios.get('https://localhost:7120/quotes');
+
+      setQuotes(prev => (resp.data))
+    }
+    dataFetch();
+  }, [])
+  console.log("I rendered")
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Text>Welcome to Mantine!</Text>
-    </MantineProvider>
+    <>
+      <header>
+        <h1 className="headerText">Peritus</h1>
+      </header>
+      <div className="App">
+        <AddQuote />
+        <div className="Content">
+          <QuoteList datalist={quotes} />
+        </div>
+      </div>
+      <footer>
+        <small>Created by Carl Morisbak Olsson @2022 </small>
+      </footer>
+    </>
   );
 }
+
+export default App;
