c58de4e0d7b7e428cca59668873479fdcc616861
SpookySnek <carleric.olsson@gmail.com>
2022-09-19 11:54:05 +0200

    Implemented AddQuote functionality

--- src/components/addQuote/addQuote.tsx
+++ src/components/addQuote/addQuote.tsx
@@ -1,3 +1,32 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import { nanoid } from 'nanoid'
 import { Quote } from '../app/App'
+
+const AddQuote = () => {
+
+    const [quote, addQuote] = useState<Quote>();
+    const [quoteText, setQuoteText] = useState<string>("");
+  
+    const handleAdd = async () => {
+      await axios.post("https://localhost:7120/quotes", {
+        "quoteText": quoteText,
+        "uploadDate": new Date(),
+        "id": nanoid(14)
+      })
+      window.location.reload()
+    }
+  
+  
+  
+    console.log(`quote ${quote}`);
+  
+    return (
+      <div>
+        <input type="text" value={quote?.quoteText} onChange={(e) => setQuoteText(e.target.value)} placeholder="Text" />
+        <button className="addButton" onClick={handleAdd}>Add quote</button>
+      </div>
+    )
+  }
+  
+  export default AddQuote
